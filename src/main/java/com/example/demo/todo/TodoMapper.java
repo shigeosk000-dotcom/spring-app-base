2d981c23ce30577a13ed1945de9f4f562d293e98
DESKTOP-LEN63T4\academia <shige.osk.000@gmail.com>
2026-04-10 15:58:12 +0900

    不具合修正

--- src/main/java/com/example/demo/todo/TodoMapper.java
+++ src/main/java/com/example/demo/todo/TodoMapper.java
@@ -1,4 +1,3 @@
-
 package com.example.demo.todo;
 
 import java.util.List;
@@ -10,16 +9,17 @@ import org.apache.ibatis.annotations.Param;
 public interface TodoMapper {
     List<Todo> findAll();
 
-    List<Todo> findByUserId(@Param( userId) String userId);
+    List<Todo> findByUserId(@Param("userId") String userId);
 
-    Todo findById(@Param( id) Long id);
-    Todo findByIdAndUserId(@Param( id) Long id, @Param(userId) String userId);
+    Todo findById(@Param("id") Long id);
+
+    Todo findByIdAndUserId(@Param("id") Long id, @Param("userId") String userId);
 
     int insert(Todo todo);
 
     int update(Todo todo);
 
-    int delete(@Param( id) Long id);
+    int delete(@Param("id") Long id);
 
-    int changeStatus(@Param( id) Long id, @Param(status) boolean status);
+    int changeStatus(@Param("id") Long id, @Param("status") boolean status);
 }
